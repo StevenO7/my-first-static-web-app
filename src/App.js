f9c9e7780cd7e40a32a3e04de07c94c345a7d77a
Steven Zhang <76230272+StevenO7@users.noreply.github.com>
2023-11-15 22:40:04 -0500

    Update App.js
    Test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'FUCK SHIT GUYS AZURE IS SHIT';
-  return <div>Hello Bye {value} some update !! why not updating!!!!! HELP!!!!</div>;
+  return <div>Lucky ScaryHello Bye {value} some update !! why not updating!!!!! HELP!!!!</div>;
 }
 
 export default App;
