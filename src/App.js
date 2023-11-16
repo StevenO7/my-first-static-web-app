a45d72c603cbafbcb153fe0a9a21338e16bab3c2
Steven Zhang <76230272+StevenO7@users.noreply.github.com>
2023-11-15 22:18:10 -0500

    Steven test 3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello Bye {value} some update !! why not updating!!!!! HELP!!!! Lucky scary</div>;
+  return <div> Steven{value} some update !! why not updating!!!!! HELP!!!! Lucky scary</div>;
 }
 
 export default App;
